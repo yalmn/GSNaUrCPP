e16bc5986bbd98701b9d893626382d1ce63cb659
Halil Yalman <halil@yalmn.dev>
2023-12-03 19:50:02 +0100

    Initial Commit

---
+++ Headers/Methoden.h
@@ -0,0 +1,16 @@
+#ifndef METHODEN_D
+#define METHODEN_D
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Methoden {
+
+public:
+  static bool isEven(int n) { return n % 2 == 0; }
+};
+
+#endif // !METHODEN_D
---
+++ Sources/App.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+#include "../Headers/Methoden.h"
+
+using namespace std;
+
+int main() {
+
+  int m = 5;
+  int *p = &m;
+
+  cout << p << endl;
+
+  return 0;
+}
